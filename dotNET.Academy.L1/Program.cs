3f0d1dc28e77ecb6ae9f71d23b8592c7877b52d3
Allkman <algirdas.cern@gmail.com>
2021-08-03 21:32:31 +0300

    Pirma C# paskaita

--- dotNET.Academy.L1/Program.cs
+++ dotNET.Academy.L1/Program.cs
@@ -2,6 +2,7 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
+using System.Threading;
 using System.Threading.Tasks;
 
 namespace dotNET.Academy.L1
@@ -10,6 +11,54 @@ namespace dotNET.Academy.L1
     {
         static void Main(string[] args)
         {
+            //UserInputTest();
+            Console.WriteLine("|-------------------------------------------------------------|");
+            Console.WriteLine();
+
+            Console.Write("Iveskite pirma skaiciu: ");
+            double a = double.Parse(Console.ReadLine());
+
+            Console.Write("Iveskite antra skaiciu: ");
+            double b = double.Parse(Console.ReadLine());
+
+            double suma = a + b;
+            double skirtumas = a - b;
+            double sandauga = a * b;
+            double dalmuo = a / b;
+
+            Console.WriteLine($"{a} + {b} = {suma}");
+            Console.WriteLine($"{a} - {b} = {skirtumas}");
+            Console.WriteLine($"{a} x {b} = {sandauga}");
+            Console.WriteLine($"{a} / {b} = {dalmuo}");
+
+            Console.ReadLine();
+        }
+        private static void UserInputTest()
+        {
+            string name = "Algirdas";
+            string surName = "Cernevicius";
+            string hobby = "Programing";
+            int age = 36;
+            string city = "Vilnius";
+            Console.WriteLine(
+                $"Hello all,\nMy name is {name} {surName}.\nMy hobby is {hobby}.\nI am {age} years old.\nI am from {city}.");
+            Console.WriteLine("|-------------------------------------------------------------|");
+            Console.WriteLine();
+
+            Console.Write("Iveskite savo varda: ");
+            string name02 = Console.ReadLine();
+            Console.Clear();
+            Console.Write("Iveskite savo amziu: ");
+            int age02 = int.Parse(Console.ReadLine());
+            Console.Clear();
+
+            Console.Write("Iveskite savo miesta: ");
+            string city02 = Console.ReadLine();
+            Console.Clear();
+            Thread.Sleep(2000);
+
+            Console.WriteLine(
+                $"Hello all,\nName is {name02}.\nAge {age02}.\nCity {city02}.");
         }
     }
 }
